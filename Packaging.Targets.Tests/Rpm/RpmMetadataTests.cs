ec7b3ea38f8ad878b7147a4646cbfa911c53b66a
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-05-02 00:16:01 +0200

    Preliminary support for RPM metadata.

---
+++ Packaging.Targets.Tests/Rpm/RpmMetadataTests.cs
@@ -0,0 +1,61 @@
+﻿using Packaging.Targets.Rpm;
+using System.Collections.ObjectModel;
+using System.IO;
+using Xunit;
+
+namespace Packaging.Targets.Tests.Rpm
+{
+    /// <summary>
+    /// Tests the <see cref="RpmMetadata"/> class.
+    /// </summary>
+    public class RpmMetadataTests
+    {
+        /// <summary>
+        /// Tests the various properties of the <see cref="RpmMetadata"/> class.
+        /// </summary>
+        [Fact]
+        public void MetadataPropertiesTest()
+        {
+
+            using (Stream stream = File.OpenRead(@"Rpm\libplist-2.0.1.151-1.1.x86_64.rpm"))
+            {
+                var package = RpmPackageReader.Read(stream);
+                var metadata = new RpmMetadata(package);
+                var headerRecords = package.Header.Records;
+
+                Assert.Equal(package, metadata.Package);
+
+                Assert.Equal("x86_64", metadata.Arch);
+                Assert.Equal("lamb11", metadata.BuildHost);
+                Assert.Equal(0x58fa71fc, metadata.BuildTime);
+                Assert.Equal("lamb11 1492808188", metadata.Cookie);
+                Assert.Equal("libplist is a library for manipulating Apple Binary and XML Property Lists", metadata.Description);
+                Assert.Equal("home:qmfrederik / CentOS_7", metadata.Distribution);
+                Assert.Equal("obs://build.opensuse.org/home:qmfrederik/CentOS_7/adfeea138cd469466e6fa13a3c88fb8f-libplist", metadata.DistUrl);
+                Assert.Equal(8, metadata.FileDigetsAlgo);
+                Assert.Equal("System Environment/Libraries", metadata.Group);
+                Assert.Equal(-976, metadata.ImmutableRegionSize);
+                Assert.Equal("LGPLv2+", metadata.License);
+                Assert.Equal(new Collection<string>() { "C" }, metadata.Locales);
+                Assert.Equal("libplist", metadata.Name);
+                Assert.Equal("-O2 -g -pipe -Wall -Wp,-D_FORTIFY_SOURCE=2 -fexceptions -fstack-protector-strong --param=ssp-buffer-size=4 -grecord-gcc-switches   -m64 -mtune=generic", metadata.OptFlags);
+                Assert.Equal("linux", metadata.Os);
+                Assert.Equal("xz", metadata.PayloadCompressor);
+                Assert.Equal("2", metadata.PayloadFlags);
+                Assert.Equal("cpio", metadata.PayloadFormat);
+                Assert.Equal("x86_64-redhat-linux-gnu", metadata.Platform);
+                Assert.Equal("/sbin/ldconfig", metadata.PostInProg);
+                Assert.Equal("/sbin/ldconfig", metadata.PostUnProg);
+                Assert.Equal("1.1", metadata.Release);
+                Assert.Equal("4.11.3", metadata.RpmVersion);
+                Assert.Equal(0x26e6d, metadata.Size);
+                Assert.Equal(new byte[] { 0x45, 0xc0, 0x86, 0x80, 0x77, 0x4e, 0xf4, 0xc0, 0x37, 0xf1, 0x1e, 0xb1, 0xd3, 0x47, 0xf0, 0xbf }, metadata.SourcePkgId);
+                Assert.Equal("libplist-2.0.1.151-1.1.src.rpm", metadata.SourceRpm);
+                Assert.Equal("Library for manipulating Apple Binary and XML Property Lists", metadata.Summary);
+                Assert.Equal("http://www.libimobiledevice.org/", metadata.Url);
+                Assert.Equal("obs://build.opensuse.org/home:qmfrederik", metadata.Vendor);
+                Assert.Equal("2.0.1.151", metadata.Version);
+            }
+        }
+    }
+}
---
+++ Packaging.Targets/Rpm/RpmMetadata.cs
@@ -0,0 +1,341 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Packaging.Targets.Rpm
+{
+    /// <summary>
+    /// Provides access to the metadata stored in the RPM package.
+    /// </summary>
+    internal class RpmMetadata
+    {
+        public RpmMetadata(RpmPackage package)
+        {
+            if (package == null)
+            {
+                throw new ArgumentNullException(nameof(package));
+            }
+
+            this.Package = package;
+        }
+
+        public RpmPackage Package
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the length of the immutable region, starting from the position of the <see cref="IndexTag.RPMTAG_HEADERIMMUTABLE"/>
+        /// record. This record should be the last record in the signature block, and the value is negative, indicating the previous
+        /// blocks are considered immutable.
+        /// </summary>
+        public int ImmutableRegionSize
+        {
+            get
+            {
+                // For more information about immutable regions, see:
+                // http://ftp.rpm.org/api/4.4.2.2/hregions.html
+                // https://dentrassi.de/2016/04/15/writing-rpm-files-in-plain-java/
+                // https://blog.bethselamin.de/posts/argh-pm.html
+                // For now, we're always assuming the entire header and the entire signature is immutable
+
+                var immutableSignatureRegion = (byte[])this.Package.Header.Records[IndexTag.RPMTAG_HEADERIMMUTABLE].Value;
+
+                using (MemoryStream s = new MemoryStream(immutableSignatureRegion))
+                {
+                    var h = s.ReadStruct<IndexHeader>();
+                    return h.Offset;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets a list of all locales supported by this package. The default, invariant locale is marked as the <c>C</c> locale.
+        /// </summary>
+        public Collection<string> Locales
+        {
+            get
+            {
+                return this.GetStringArray(IndexTag.RPMTAG_HEADERI18NTABLE);
+            }
+        }
+
+        /// <summary>
+        /// Gets the name of the package.
+        /// </summary>
+        public string Name
+        {
+            get { return this.GetString(IndexTag.RPMTAG_NAME); }
+        }
+
+        /// <summary>
+        /// Gets the package version.
+        /// </summary>
+        public string Version
+        {
+            get { return this.GetString(IndexTag.RPMTAG_VERSION); }
+        }
+
+        /// <summary>
+        /// Gets the release number of the package.
+        /// </summary>
+        public string Release
+        {
+            get { return this.GetString(IndexTag.RPMTAG_RELEASE); }
+        }
+
+        /// <summary>
+        /// Gets the a summary (one line) description of the package.
+        /// </summary>
+        public string Summary
+        {
+            get { return this.GetLocalizedString(IndexTag.RPMTAG_SUMMARY); }
+        }
+
+        /// <summary>
+        /// Gets the full description of the package.
+        /// </summary>
+        public string Description
+        {
+            get { return this.GetLocalizedString(IndexTag.RPMTAG_DESCRIPTION); }
+        }
+
+        /// <summary>
+        /// Gets the date and time at which the package was built.
+        /// </summary>
+        public int BuildTime
+        {
+            get { return this.GetInt(IndexTag.RPMTAG_BUILDTIME); }
+        }
+
+        /// <summary>
+        /// Gets the name of the host on which the package was built.
+        /// </summary>
+        public string BuildHost
+        {
+            get { return this.GetString(IndexTag.RPMTAG_BUILDHOST); }
+        }
+
+        /// <summary>
+        /// Gets the sum of the sizes of the regular files in the package.
+        /// </summary>
+        public int Size
+        {
+            get { return this.GetInt(IndexTag.RPMTAG_SIZE); }
+        }
+
+        /// <summary>
+        /// Gets the name of the distribution for which the package was built.
+        /// </summary>
+        public string Distribution
+        {
+            get { return this.GetString(IndexTag.RPMTAG_DISTRIBUTION); }
+        }
+
+        /// <summary>
+        /// Gets the name of the organization which produced the package.
+        /// </summary>
+        public string Vendor
+        {
+            get { return this.GetString(IndexTag.RPMTAG_VENDOR); }
+        }
+
+        /// <summary>
+        /// Gets the license which applies to this package.
+        /// </summary>
+        public string License
+        {
+            get { return this.GetString(IndexTag.RPMTAG_LICENSE); }
+        }
+
+        /// <summary>
+        /// Gets the administrative group to which this package belongs.
+        /// </summary>
+        public string Group
+        {
+            get { return this.GetLocalizedString(IndexTag.RPMTAG_GROUP); }
+        }
+
+        /// <summary>
+        /// Gets the generic package information URL.
+        /// </summary>
+        public string Url
+        {
+            get { return this.GetString(IndexTag.RPMTAG_URL); }
+        }
+
+        /// <summary>
+        /// Gets the generic name of the OS for which this package was built. Should be <c>linux</c>.
+        /// </summary>
+        public string Os
+        {
+            get { return this.GetString(IndexTag.RPMTAG_OS); }
+        }
+
+        /// <summary>
+        /// Gets the name of the architecture for which the package was built, as defined in the architecture-specific
+        /// LSB specifications.
+        /// </summary>
+        public string Arch
+        {
+            get { return this.GetString(IndexTag.RPMTAG_ARCH); }
+        }
+
+        /// <summary>
+        /// Gets the name of the source RPM.
+        /// </summary>
+        public string SourceRpm
+        {
+            get { return this.GetString(IndexTag.RPMTAG_SOURCERPM); }
+        }
+
+        /// <summary>
+        /// Gets the version of the RPM tool used to build this package.
+        /// </summary>
+        public string RpmVersion
+        {
+            get { return this.GetString(IndexTag.RPMTAG_RPMVERSION); }
+        }
+
+        /// <summary>
+        /// Gets the name of the program to run after installation of this package.
+        /// </summary>
+        public string PostInProg
+        {
+            get { return this.GetString(IndexTag.RPMTAG_POSTINPROG); }
+        }
+
+        /// <summary>
+        /// Gets the name of the program to run after removal of this package.
+        /// </summary>
+        public string PostUnProg
+        {
+            get { return this.GetString(IndexTag.RPMTAG_POSTUNPROG); }
+        }
+
+        /// <summary>
+        /// Gets an opaque string whose contents are undefined.
+        /// </summary>
+        public string Cookie
+        {
+            get { return this.GetString(IndexTag.RPMTAG_COOKIE); }
+        }
+
+        /// <summary>
+        /// Gets additional flags which may have been passed to the compiler when building this package.
+        /// </summary>
+        public string OptFlags
+        {
+            get { return this.GetString(IndexTag.RPMTAG_OPTFLAGS); }
+        }
+
+        /// <summary>
+        /// Gets the URL for the package.
+        /// </summary>
+        public string DistUrl
+        {
+            get { return this.GetString(IndexTag.RPMTAG_DISTURL); }
+        }
+
+        /// <summary>
+        /// Gets the format of the payload. Should be <c>cpio.</c>
+        /// </summary>
+        public string PayloadFormat
+        {
+            get { return this.GetString(IndexTag.RPMTAG_PAYLOADFORMAT); }
+        }
+
+        /// <summary>
+        /// Gets the name of the compressor used to compress the payload.
+        /// </summary>
+        public string PayloadCompressor
+        {
+            get { return this.GetString(IndexTag.RPMTAG_PAYLOADCOMPRESSOR); }
+        }
+
+        /// <summary>
+        /// Gets the compression level used for the payload.
+        /// </summary>
+        public string PayloadFlags
+        {
+            get { return this.GetString(IndexTag.RPMTAG_PAYLOADFLAGS); }
+        }
+
+        /// <summary>
+        /// Gets an opaque string whose value is undefined.
+        /// </summary>
+        public string Platform
+        {
+            get { return this.GetString(IndexTag.RPMTAG_PLATFORM); }
+        }
+
+        public byte[] SourcePkgId
+        {
+            get { return this.GetByteArray(IndexTag.RPMTAG_SOURCEPKGID); }
+        }
+
+        public int FileDigetsAlgo
+        {
+            get { return this.GetInt(IndexTag.RPMTAG_FILEDIGESTALGO); }
+        }
+
+        // Changelog
+        // Require_*: Dependencies
+        // Provide_*: Dependencies
+        // Dirs
+        // Files
+
+        protected Collection<string> GetStringArray(IndexTag tag)
+        {
+            return this.GetValue<Collection<string>>(tag, IndexType.RPM_STRING_ARRAY_TYPE, false);
+        }
+
+        protected string GetLocalizedString(IndexTag tag)
+        {
+            var localizedValues = this.GetValue<Collection<string>>(tag, IndexType.RPM_I18NSTRING_TYPE, false);
+
+            return localizedValues.First();
+        }
+
+        protected string GetString(IndexTag tag)
+        {
+            return this.GetValue<string>(tag, IndexType.RPM_STRING_TYPE, false);
+        }
+
+        protected int GetInt(IndexTag tag)
+        {
+            return this.GetValue<int>(tag, IndexType.RPM_INT32_TYPE, false);
+        }
+
+        protected byte[] GetByteArray(IndexTag tag)
+        {
+            return this.GetValue<byte[]>(tag, IndexType.RPM_BIN_TYPE, true);
+        }
+
+        protected T GetValue<T>(IndexTag tag, IndexType type, bool plural)
+        {
+            if (!this.Package.Header.Records.ContainsKey(tag))
+            {
+                throw new ArgumentOutOfRangeException(nameof(tag));
+            }
+
+            var record = this.Package.Header.Records[tag];
+
+            if (record.Header.Count > 1 != plural)
+            {
+                throw new ArgumentOutOfRangeException(nameof(tag));
+            }
+
+            if (record.Header.Type != type)
+            {
+                throw new ArgumentOutOfRangeException(nameof(tag));
+            }
+
+            return (T)record.Value;
+        }
+    }
+}
