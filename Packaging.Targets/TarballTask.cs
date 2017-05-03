6978d462beac72d17855a2a25448ac1bd9ec6008
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-05-03 23:05:05 +0200

    Update dotnet-zip, dotnet-tarball targets

--- Packaging.Targets/TarballTask.cs
+++ Packaging.Targets/TarballTask.cs
@@ -3,6 +3,7 @@ using ICSharpCode.SharpZipLib.Tar;
 using ICSharpCode.SharpZipLib.Zip;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
+using System;
 using System.Diagnostics;
 using System.IO;
 
@@ -17,19 +18,8 @@ namespace Packaging.Targets
             set;
         }
 
+        [Required]
         public string TarballPath
-        {
-            get;
-            set;
-        }
-
-        public string TargetRuntime
-        {
-            get;
-            set;
-        }
-
-        public string TargetFramework
         {
             get;
             set;
@@ -39,41 +29,10 @@ namespace Packaging.Targets
         {
             this.Log.LogMessage(MessageImportance.Normal, "Creating tarball '{0}' from folder '{1}'", this.TarballPath, this.PublishDir);
 
-            CreateWindowsTarball();
+            CreateLinuxTarball();
 
             this.Log.LogMessage(MessageImportance.Normal, "Created tarball '{0}' from folder '{1}'", this.TarballPath, this.PublishDir);
             return true;
-        }
-
-        private void CreateWindowsTarball()
-        {
-            using (var stream = File.Create(this.TarballPath))
-            using (var zipFile = ZipFile.Create(stream))
-            {
-                AddDirectory(zipFile, this.PublishDir, string.Empty);
-            }
-        }
-
-        private void AddDirectory(ZipFile zipFile, string directory, string directoryEntryName)
-        {
-            if (directoryEntryName != string.Empty)
-            {
-                zipFile.BeginUpdate();
-                zipFile.AddDirectory(directoryEntryName);
-                zipFile.CommitUpdate();
-            }
-
-            foreach (var file in Directory.GetFiles(directory))
-            {
-                zipFile.BeginUpdate();
-                zipFile.Add(file, Path.Combine(directoryEntryName, Path.GetFileName(file)));
-                zipFile.CommitUpdate();
-            }
-
-            foreach(var child in Directory.GetDirectories(directory))
-            {
-                AddDirectory(zipFile, child, Path.Combine(directoryEntryName, Path.GetFileName(child)));
-            }
         }
 
         private void CreateLinuxTarball()
@@ -82,7 +41,9 @@ namespace Packaging.Targets
             using (var gzipStream = new GZipOutputStream(stream))
             using (var archive = TarArchive.CreateOutputTarArchive(gzipStream))
             {
+                archive.RootPath = this.PublishDir.Replace("\\", "/");
                 var entry = TarEntry.CreateEntryFromFile(this.PublishDir);
+
                 archive.WriteEntry(entry, true);
             }
         }
