142d62877abad89bbbbd18a9bf9c84d4271c3835
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-05-11 00:35:36 +0200

    Add directory entries, fix order in which entries are written out.

--- Packaging.Targets/IO/CpioFileCreator.cs
+++ Packaging.Targets/IO/CpioFileCreator.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace Packaging.Targets.IO
@@ -30,60 +31,88 @@ namespace Packaging.Targets.IO
         {
             byte[] fileHeader = new byte[1024];
 
-            foreach (var file in Directory.GetFiles(directory))
+            // Write out an entry for the current directory
+            CpioHeader directoryHeader = new CpioHeader()
             {
+                Check = 0,
+                DevMajor = 1,
+                DevMinor = 0,
+                FileSize = 0,
+                Gid = 0,
+                Ino = inode,
+                Mode = LinuxFileMode.S_IXOTH | LinuxFileMode.S_IROTH | LinuxFileMode.S_IXGRP | LinuxFileMode.S_IRGRP | LinuxFileMode.S_IXUSR | LinuxFileMode.S_IWUSR | LinuxFileMode.S_IRUSR | LinuxFileMode.S_IFDIR,
+                Mtime = Directory.GetLastWriteTime(directory),
+                Nlink = 1,
+                RDevMajor = 0,
+                RDevMinor = 0,
+                Signature = "070701",
+                Uid = 0,
+                NameSize = 0
+            };
 
-                using (Stream fileStream = File.OpenRead(file))
+            cpioFile.Write(directoryHeader, prefix, new MemoryStream());
+
+            // The other in which the files appear in the cpio archive is important; if this is not respected xzdio
+            // will report errors like:
+            // error: unpacking of archive failed on file ./usr/share/quamotion/mscorlib.dll: cpio: Archive file not in header
+            var entries = Directory.GetFileSystemEntries(directory).OrderBy(e => e, StringComparer.Ordinal).ToArray();
+
+            foreach (var entry in entries)
+            {
+                if (File.Exists(entry))
                 {
-                    var fileName = Path.GetFileName(file);
-
-                    if (fileName.StartsWith(".") || fileStream.Length == 0)
+                    using (Stream fileStream = File.OpenRead(entry))
                     {
-                        // Skip hidden and empty files - this would case rmplint errors.
-                        continue;
+                        var fileName = Path.GetFileName(entry);
+
+                        if (fileName.StartsWith(".") || fileStream.Length == 0)
+                        {
+                            // Skip hidden and empty files - this would case rmplint errors.
+                            continue;
+                        }
+
+                        inode++;
+
+                        var read = fileStream.Read(fileHeader, 0, fileHeader.Length);
+
+                        bool isExecutable = this.fileAnayzer.IsExecutable(fileHeader);
+
+                        var mode = LinuxFileMode.S_IROTH | LinuxFileMode.S_IRGRP | LinuxFileMode.S_IRUSR | LinuxFileMode.S_IFREG;
+
+                        if (isExecutable)
+                        {
+                            mode |= LinuxFileMode.S_IXOTH | LinuxFileMode.S_IXGRP | LinuxFileMode.S_IWUSR | LinuxFileMode.S_IXUSR;
+                        }
+
+                        fileStream.Position = 0;
+                        string name = prefix + "/" + fileName;
+
+                        CpioHeader cpioHeader = new CpioHeader()
+                        {
+                            Check = 0,
+                            DevMajor = 1,
+                            DevMinor = 0,
+                            FileSize = (uint)fileStream.Length,
+                            Gid = 0, // root
+                            Uid = 0, // root
+                            Ino = inode,
+                            Mode = mode,
+                            Mtime = File.GetLastWriteTimeUtc(entry),
+                            NameSize = (uint)name.Length + 1,
+                            Nlink = 1,
+                            RDevMajor = 0,
+                            RDevMinor = 0,
+                            Signature = "070701",
+                        };
+
+                        cpioFile.Write(cpioHeader, name, fileStream);
                     }
-
-                    inode++;
-
-                    var read = fileStream.Read(fileHeader, 0, fileHeader.Length);
-
-                    bool isExecutable = this.fileAnayzer.IsExecutable(fileHeader);
-
-                    var mode = LinuxFileMode.S_IROTH | LinuxFileMode.S_IRGRP | LinuxFileMode.S_IRUSR | LinuxFileMode.S_IFREG;
-
-                    if (isExecutable)
-                    {
-                        mode |= LinuxFileMode.S_IXOTH | LinuxFileMode.S_IXGRP | LinuxFileMode.S_IWUSR | LinuxFileMode.S_IXUSR;
-                    }
-
-                    fileStream.Position = 0;
-                    string name = prefix + "/" + fileName;
-
-                    CpioHeader cpioHeader = new CpioHeader()
-                    {
-                        Check = 0,
-                        DevMajor = 1,
-                        DevMinor = 0,
-                        FileSize = (uint)fileStream.Length,
-                        Gid = 0, // root
-                        Uid = 0, // root
-                        Ino = inode,
-                        Mode = mode,
-                        Mtime = File.GetLastWriteTimeUtc(file),
-                        NameSize = (uint)name.Length + 1,
-                        Nlink = 1,
-                        RDevMajor = 0,
-                        RDevMinor = 0,
-                        Signature = "070701",
-                    };
-
-                    cpioFile.Write(cpioHeader, name, fileStream);
                 }
-            }
+                else
+                {
 
-            foreach (var child in Directory.GetDirectories(directory))
-            {
-                AddDirectory(child, prefix + "/" + Path.GetFileName(child), cpioFile, ref inode);
+                    AddDirectory(entry, prefix + "/" + Path.GetFileName(entry), cpioFile, ref inode);
+                }
             }
         }
     }
