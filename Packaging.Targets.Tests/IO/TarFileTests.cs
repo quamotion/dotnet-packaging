21f393c69af32d0ed9548aaf6ecd308216ef187c
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-05-14 22:20:18 +0200

    Preliminary support for reading tar files

---
+++ Packaging.Targets.Tests/IO/TarFileTests.cs
@@ -0,0 +1,51 @@
+﻿using Packaging.Targets.IO;
+using System.Collections.ObjectModel;
+using System.IO;
+using Xunit;
+
+namespace Packaging.Targets.Tests.IO
+{
+    /// <summary>
+    /// Tests the <see cref="TarFile"/> class.
+    /// </summary>
+    public class TarFileTests
+    {
+        /// <summary>
+        /// Reads the contents of a <c>.tar</c> file.
+        /// </summary>
+        [Fact]
+        public void ReadTarFileTest()
+        {
+            using (Stream stream = File.OpenRead("Deb/libplist3_1.12-3.1_amd64.deb"))
+            using (ArFile arFile = new ArFile(stream, leaveOpen: true))
+            {
+                // Skip the debian version
+                arFile.Read();
+
+                // This is the tar file
+                arFile.Read();
+
+                Collection<string> filenames = new Collection<string>();
+                Collection<string> contents = new Collection<string>();
+                Collection<TarHeader> headers = new Collection<TarHeader>();
+
+                using (Stream entryStream = arFile.Open())
+                using (GZipDecompressor decompressedStream = new GZipDecompressor(entryStream, leaveOpen: true))
+                using (TarFile tarFile = new TarFile(decompressedStream, leaveOpen: true))
+                {
+                    while (tarFile.Read())
+                    {
+                        filenames.Add(tarFile.FileName);
+                        headers.Add((TarHeader)tarFile.FileHeader);
+
+                        using (Stream data = tarFile.Open())
+                        using (StreamReader reader = new StreamReader(data))
+                        {
+                            contents.Add(reader.ReadToEnd());
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Packaging.Targets/IO/TarTypeFlag.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Packaging.Targets.IO
+{
+    internal enum TarTypeFlag : byte
+    {
+        RegType = (byte)'0',
+        ARegType = (byte)'\0',
+        LnkType = (byte)'1',
+        SymType = (byte)'2',
+        ChrType = (byte)'3',
+        BlkType = (byte)'4',
+        DirType = (byte)'5',
+        FifoType = (byte)'6',
+        ConttType = (byte)'7',
+        ExtendedHeader = (byte)'x',
+        GlobalExtendedHeader = (byte)'g'
+    }
+}
