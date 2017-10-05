c4294af085179c5137379a614dd9cb05bc950ee8
Nikita Tsukanov <keks9n@gmail.com>
2017-10-06 01:02:00 +0300

    Implemented ArFileCreator

--- Packaging.Targets.Tests/IO/ArFileTests.cs
+++ Packaging.Targets.Tests/IO/ArFileTests.cs
@@ -1,6 +1,8 @@
 ﻿using Packaging.Targets.IO;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using Xunit;
 
 namespace Packaging.Targets.Tests.IO
@@ -38,6 +40,61 @@ namespace Packaging.Targets.Tests.IO
                         "data.tar.xz"
                     },
                     filenames);
+            }
+        }
+
+        ArHeader CloneHeader(ArHeader hdr)
+        {
+            return new ArHeader
+            {
+                EndChar = hdr.EndChar,
+                FileMode = hdr.FileMode,
+                FileName = hdr.FileName,
+                FileSize = hdr.FileSize,
+                GroupId = hdr.GroupId,
+                OwnerId = hdr.OwnerId,
+                LastModified = hdr.LastModified
+            };
+        }
+        
+        void AssertCompareClonedHeader(ArHeader original, ArHeader clone)
+        {
+            var ms = new MemoryStream();
+            ms.WriteStruct(clone);
+            ms.Seek(0, SeekOrigin.Begin);
+            clone = ms.ReadStruct<ArHeader>();
+            foreach (var f in typeof(ArHeader).GetFields(BindingFlags.Instance | BindingFlags.NonPublic |
+                                                          BindingFlags.Public))
+            {
+                var orig = f.GetValue(original);
+                var mod = f.GetValue(clone);
+                if (mod is byte[] modchars)
+                    Assert.True(modchars.SequenceEqual((byte[]) orig), $"Failed check for {f.Name}");
+                else
+                    Assert.True(orig.Equals(mod), $"Failed check for {f.Name}");
+            }
+        }
+        
+
+        
+        [Fact]
+        public void WriteTest()
+        {
+            using (Stream original = File.OpenRead("Deb/libplist3_1.12-3.1_amd64.deb"))
+            using (Stream expected = File.OpenRead("Deb/libplist3_1.12-3.1_amd64.deb"))
+            using (Stream actual = new MemoryStream())
+            using (Stream output = new ValidatingCompositeStream(null, actual, expected))
+            {
+                ArFileCreator.WriteMagic(output);
+                var input = new ArFile(original, true);
+                while (input.Read())
+                {
+                    var header = (ArHeader) input.FileHeader;
+                    var clone = CloneHeader(header);
+                    AssertCompareClonedHeader(header, clone);
+                    using(var data = input.Open())
+                        ArFileCreator.WriteEntry(output, clone, data);
+                }
             }
         }
     }
---
+++ Packaging.Targets/IO/ArFileCreator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Packaging.Targets.IO
+{
+    public static class ArFileCreator
+    {
+        public static void WriteMagic(Stream output)
+        {
+            var wr = new StreamWriter(output);
+            wr.Write("!<arch>\n");
+            wr.Flush();
+        }
+
+        public static void WriteEntry(Stream output, string name, LinuxFileMode mode, Stream data)
+        {
+            var hdr = new ArHeader
+            {
+                EndChar = "`\n",
+                FileMode = mode,
+                FileName = name,
+                FileSize = (uint)data.Length,
+                GroupId = 0,
+                OwnerId = 0,
+                LastModified = DateTimeOffset.UtcNow
+            };
+            WriteEntry(output, hdr, data);
+        }
+        
+        public static void WriteEntry(Stream output, ArHeader header, Stream data)
+        {
+            output.WriteStruct(header);
+            data.CopyTo(output);
+            if (output.Position % 2 != 0)
+                output.WriteByte(0);
+        }
+    }
+}
