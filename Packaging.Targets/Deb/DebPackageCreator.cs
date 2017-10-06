0a305c87e3d821caec5ab984b38805dc986c57a1
Nikita Tsukanov <keks9n@gmail.com>
2017-10-06 04:11:44 +0300

    [DEB] Added support for RemoveOnUninstall

--- Packaging.Targets/Deb/DebPackageCreator.cs
+++ Packaging.Targets/Deb/DebPackageCreator.cs
@@ -55,6 +55,12 @@ namespace Packaging.Targets.Deb
                 pkg.PostInstallScript += $"systemctl enable --now {serviceName}.service";
                 pkg.PreRemoveScript += $"systemctl --no-reload disable --now {serviceName}.service";
             }
+            
+            // Remove all directories marked as such (these are usually directories which contain temporary files)
+            foreach (var entryToRemove in archiveEntries.Where(e => e.RemoveOnUninstall))
+            {
+                pkg.PostRemoveScript += $"/usr/bin/rm -rf {entryToRemove.TargetPath}\n";
+            }
 
             //Dependency list from https://github.com/dotnet/dotnet-docker/blob/master/2.0/runtime-deps/jessie/amd64/Dockerfile
             string deps = "libc6, libcurl3, libgcc1, libgssapi-krb5-2, libicu52, liblttng-ust0, libssl1.0.0, libstdc++6, libunwind8, libuuid1, zlib1g";
