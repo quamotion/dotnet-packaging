85ddadef191e059ce8fb16ce5659a37ebfac0883
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-10-12 23:48:37 +0200

    Make package creation more flexible: - Allow faking the PGP signatures (useful for unit tests) - Allow including the version name in RPM name in the RPM lead - Don't populate prein/postin/preout/postout keys if no scripts are present - Allow passing a compressed payload stream (useful for unit tests)

---
+++ Packaging.Targets/Rpm/IPackageSigner.cs
@@ -0,0 +1,13 @@
+﻿using Org.BouncyCastle.Bcpg.OpenPgp;
+using System.IO;
+
+namespace Packaging.Targets.Rpm
+{
+    /// <summary>
+    /// Common interface for any class that can generate PGP signatures of a <see cref="Stream"/>.
+    /// </summary>
+    internal interface IPackageSigner
+    {
+        PgpSignature Sign(Stream payload);
+    }
+}
---
+++ Packaging.Targets/Rpm/PackageSigner.cs
@@ -0,0 +1,30 @@
+﻿using Org.BouncyCastle.Bcpg.OpenPgp;
+using System;
+using System.IO;
+
+namespace Packaging.Targets.Rpm
+{
+    /// <summary>
+    /// Generates <see cref="PgpSignature"/> values using a <see cref="Stream"/> and a
+    /// <see cref="PgpPrivateKey"/>.
+    /// </summary>
+    internal class PackageSigner : IPackageSigner
+    {
+        private readonly PgpPrivateKey privateKey;
+
+        public PackageSigner(PgpPrivateKey privateKey)
+        {
+            if (privateKey == null)
+            {
+                throw new ArgumentNullException(nameof(privateKey));
+            }
+
+            this.privateKey = privateKey;
+        }
+
+        public PgpSignature Sign(Stream payload)
+        {
+            return PgpSigner.Sign(this.privateKey, payload);
+        }
+    }
+}
