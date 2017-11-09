dd9df0d46c049964170363db60bda94ba2a412fa
Frederik Carlier <frederik.carlier@quamotion.mobi>
2017-11-09 16:22:32 +0100

    Preliminary harvesting support

---
+++ Packaging.Targets.Tests/RpmRepo/MetadataHarvesterTests.cs
@@ -0,0 +1,85 @@
+﻿using Packaging.Targets.RpmRepo;
+using Xunit;
+
+namespace Packaging.Targets.Tests.RpmRepo
+{
+    public class MetadataHarvesterTests
+    {
+        [Fact]
+        public void ExtractMetadataTest()
+        {
+            MetadataHarvester harvester = new MetadataHarvester();
+            var metadata = harvester.Harvest("RpmRepo/repo/RPMS");
+
+            Assert.Equal(2, metadata.Packages.Count);
+
+            // Primary metadata
+            var rpm = metadata.Packages[0];
+            Assert.Equal("rpm", rpm.Type);
+            Assert.Equal("src", rpm.Arch);
+            Assert.Equal(0, rpm.VersionEpoch);
+            Assert.Equal("1.2", rpm.Version);
+            Assert.Equal("1.el6", rpm.Release);
+            Assert.Equal("sha256", rpm.ChecksumType);
+            Assert.True(rpm.ChecksumIsPkgId);
+            Assert.Equal("0c318f3146cc6206d88746326d3ad86b5c53b358add9c80cabc8de5b47270e8b", rpm.Checksum);
+            Assert.Equal("Library for manipulating Apple Binary and XML Property Lists", rpm.Summary);
+            Assert.Equal("libplist is a library for manipulating Apple Binary and XML Property Lists", rpm.Description);
+            Assert.Equal("CentOS BuildSystem <http://bugs.centos.org>", rpm.Packager);
+            Assert.Equal("http://matt.colyer.name/projects/iphone-linux/", rpm.Url);
+            Assert.Equal(1323184405, rpm.FileTime);
+            Assert.Equal(1282480066, rpm.BuildTime);
+            Assert.Equal(69327, rpm.PackageSize);
+            Assert.Equal(69879, rpm.InstalledSize);
+            //Assert.Equal(70264, rpm.ArchiveSize);
+            Assert.Equal("RPMS/libplist-1.2-1.el6.src.rpm", rpm.Location);
+            Assert.Equal("LGPLv2+", rpm.License);
+            Assert.Equal("CentOS", rpm.Vendor);
+            Assert.Equal("System Environment/Libraries", rpm.Group);
+            Assert.Equal("c6b6.bsys.dev.centos.org", rpm.BuildHost);
+            Assert.Equal(string.Empty, rpm.SourceRpm);
+            Assert.Equal(1384, rpm.HeaderStart);
+            Assert.Equal(3632, rpm.HeaderEnd);
+            Assert.Equal(5, rpm.Requires.Count);
+            Assert.Equal("cmake", rpm.Requires[0].Name);
+
+            rpm = metadata.Packages[1];
+
+            Assert.Equal(4, rpm.Provides.Count);
+            Assert.Equal("libplist", rpm.Provides[0].Name);
+            Assert.Equal("EQ", rpm.Provides[0].Flags);
+            Assert.Equal(0, rpm.Provides[0].Epoch);
+            Assert.Equal("1.2", rpm.Provides[0].Version);
+            Assert.Equal("1.el6", rpm.Provides[0].Release);
+
+            Assert.Equal(2, rpm.Files.Count);
+            Assert.Equal("/usr/bin/plutil", rpm.Files[0]);
+
+            // File list
+            var package = metadata.Packages[0];
+            Assert.Equal("0c318f3146cc6206d88746326d3ad86b5c53b358add9c80cabc8de5b47270e8b", package.PkgId);
+            Assert.Equal("libplist", package.Name);
+            Assert.Equal("src", package.Arch);
+            Assert.Equal(0, package.VersionEpoch);
+            Assert.Equal("1.2", package.Version);
+            Assert.Equal("1.el6", package.Release);
+
+            Assert.Equal(2, package.Files.Count);
+            Assert.Equal("libplist-1.2.tar.bz2", package.Files[0]);
+
+            // Other metadata
+            package = metadata.Packages[0];
+            Assert.Equal("0c318f3146cc6206d88746326d3ad86b5c53b358add9c80cabc8de5b47270e8b", package.PkgId);
+            Assert.Equal("libplist", package.Name);
+            Assert.Equal("src", package.Arch);
+            Assert.Equal(0, package.VersionEpoch);
+            Assert.Equal("1.2", package.Version);
+            Assert.Equal("1.el6", package.Release);
+
+            var changelog = package.ChangeLog[0];
+            Assert.Equal("Peter Robinson <pbrobinson@gmail.com> 0.12.0-0.1", changelog.Author);
+            Assert.Equal(1241956800, changelog.Date);
+            Assert.Equal("- Initial package", changelog.Text);
+        }
+    }
+}
---
+++ Packaging.Targets/RpmRepo/MetadataHarvester.cs
@@ -0,0 +1,158 @@
+﻿using Packaging.Targets.Rpm;
+using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Packaging.Targets.RpmRepo
+{
+    /// <summary>
+    /// Harvests metadata from a physical RPM package and generates the RPM metadta files.
+    /// </summary>
+    public class MetadataHarvester
+    {
+        public PrimaryMetadata Harvest(string directory)
+        {
+            var value = new PrimaryMetadata();
+
+            foreach (var file in Directory.GetFiles(directory, "*.rpm"))
+            {
+                var metadata = this.HarvestPackage(file);
+                value.Packages.Add(metadata);
+            }
+
+            return value;
+        }
+
+        protected PackageMetadata HarvestPackage(string path)
+        {
+            using (Stream stream = File.OpenRead(path))
+            {
+                RpmPackage package = RpmPackageReader.Read(stream);
+                var metadata = new RpmMetadata(package);
+
+                // The architecture embedded in the source packages seems wrong, we get 'x86_64'
+                // for libplist-1.2-1.el6.src.rpm instead of 'src', so get it from the file name instead
+                var fileParts = path.Split('.');
+                var arch = fileParts[fileParts.Length - 2];
+
+                var packageVersion = RpmVersion.Parse(metadata.Version);
+                var fileinfo = new FileInfo(path);
+
+                PackageMetadata packageMetadata = new PackageMetadata()
+                {
+                    Arch = arch,
+                    ArchiveSize = (int)fileinfo.Length,
+                    BuildHost = metadata.BuildHost,
+                    BuildTime = metadata.BuildTime.ToUnixTimeSeconds(),
+                    Description = metadata.Description,
+                    Group = metadata.Group,
+                    HeaderStart = package.HeaderOffset,
+                    HeaderEnd = package.PayloadOffset,
+                    InstalledSize = metadata.Size,
+                    License = metadata.License,
+                    Name = metadata.Name,
+                    Packager = metadata.Packager,
+                    Release = metadata.Release,
+                    SourceRpm = metadata.SourceRpm ?? string.Empty,
+                    Summary = metadata.Summary,
+                    Url = metadata.Url,
+                    Vendor = metadata.Vendor,
+                    Version = packageVersion.Version,
+                    VersionEpoch = packageVersion.Epoch ?? 0,
+                    FileTime = new DateTimeOffset(fileinfo.LastWriteTimeUtc).ToUnixTimeSeconds(),
+                    PackageSize = fileinfo.Length,
+                    Location = $"RPMS/{fileinfo.Name}"
+                };
+
+                foreach (var changelogEntry in metadata.ChangelogEntries)
+                {
+                    packageMetadata.ChangeLog.Add(
+                        new ChangeLogEntry()
+                        {
+                            Author = changelogEntry.Name,
+                            Date = changelogEntry.Date.ToUnixTimeSeconds(),
+                            Text = changelogEntry.Text
+                        });
+                }
+
+                foreach (var file in metadata.Files)
+                {
+                    packageMetadata.Files.Add(file.Name);
+                }
+
+                foreach (var provide in metadata.Provides)
+                {
+                    var version = RpmVersion.Parse(provide.Version);
+
+                    packageMetadata.Provides.Add(
+                        new RpmEntry()
+                        {
+                            Name = provide.Name,
+                            Flags = this.GetFlag(provide.Flags),
+                            Epoch = version.Epoch ?? 0,
+                            Release = version.Release,
+                            Version = version.Version
+                        });
+                }
+
+                packageMetadata.Provides.Sort((x, y) => string.Compare(x.Name, y.Name));
+
+                foreach (var require in metadata.Requires)
+                {
+                    if (require.Flags.HasFlag(RpmSense.RPMSENSE_RPMLIB))
+                    {
+                        // These are requirements for RPM and are not included
+                        continue;
+                    }
+
+                    var version = RpmVersion.Parse(require.Version);
+
+                    var entry =
+                        new RpmEntry()
+                        {
+                            Name = require.Name,
+                            Flags = this.GetFlag(require.Flags),
+                            Epoch = version.Epoch ?? 0,
+                            Release = version.Release,
+                            Version = version.Version
+                        };
+
+                    if (require.Flags.HasFlag(RpmSense.RPMSENSE_SCRIPT_PRE))
+                    {
+                        entry.Pre = 1;
+                    }
+
+                    packageMetadata.Requires.Add(entry);
+                }
+
+                packageMetadata.Requires.Sort((x, y) => string.Compare(x.Name, y.Name));
+
+                packageMetadata.Type = "rpm";
+
+                packageMetadata.ChecksumType = "sha256";
+                packageMetadata.ChecksumIsPkgId = true;
+
+                using (var sha = SHA256.Create())
+                {
+                    stream.Position = 0;
+                    var hash = sha.ComputeHash(stream);
+                    packageMetadata.Checksum = BitConverter.ToString(hash).Replace("-", string.Empty).ToLower();
+                }
+
+                return packageMetadata;
+            }
+        }
+
+        private string GetFlag(RpmSense sense)
+        {
+            switch (sense)
+            {
+                case RpmSense.RPMSENSE_EQUAL:
+                    return "EQ";
+
+                default:
+                    return null;
+            }
+        }
+    }
+}
