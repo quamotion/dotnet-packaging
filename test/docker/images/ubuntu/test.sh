4de2cf5e92d2b91e0c7f804746f98b83e8dbbbb0
clemensv <clemensv@microsoft.com>
2018-08-29 14:45:03 +0200

    call tests

--- test/docker/images/ubuntu/test.sh
+++ test/docker/images/ubuntu/test.sh
@@ -2,6 +2,14 @@
 pushd "${0%/*}" > /dev/null 
 
 IMAGE_NAME=dotnet_packaging_ubuntu1604_test
-source ../../_scripts/imagetests.sh
+
+FDD_PACKAGE=${1##*/}
+SCD_PACKAGE=${2##*/}
+_IMAGENAME=dotnet_packaging_ubuntu1604_test
+_MOUNTPATH=%cd%
+call ..\..\_scripts\runtest.cmd %_IMAGENAME% test-cli-fdd %FDD_PACKAGE% 
+if ERRORLEVEL 1 goto end
+call ..\..\_scripts\runtest.cmd %_IMAGENAME% test-cli-scd %SCD_PACKAGE%
+
 popd
 exit $_RESULT
