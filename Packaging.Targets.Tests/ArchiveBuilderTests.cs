333273c4996229439724cf87f7c121dbe5fbcc89
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-08-30 22:53:15 +0200

    Add Travis CI build (#69)

--- Packaging.Targets.Tests/ArchiveBuilderTests.cs
+++ Packaging.Targets.Tests/ArchiveBuilderTests.cs
@@ -3,6 +3,7 @@ using Microsoft.Build.Utilities;
 using Packaging.Targets.IO;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using Xunit;
 
 namespace Packaging.Targets.Tests
@@ -31,7 +32,7 @@ namespace Packaging.Targets.Tests
             Assert.Equal(LinuxFileMode.S_IROTH |  LinuxFileMode.S_IRGRP |  LinuxFileMode.S_IRUSR | LinuxFileMode.S_IFREG, readme.Mode);
             Assert.Equal("root", readme.Owner);
             Assert.False(readme.RemoveOnUninstall);
-            Assert.Equal("archive\\README.md", readme.SourceFilename);
+            Assert.Equal(Path.Combine("archive", "README.md"), readme.SourceFilename);
             Assert.Equal("/opt/demo/README.md", readme.TargetPath);
             Assert.Equal("/opt/demo/README.md", readme.TargetPathWithFinalSlash);
             Assert.Equal(ArchiveEntryType.None, readme.Type);
@@ -44,7 +45,7 @@ namespace Packaging.Targets.Tests
             Assert.Equal(LinuxFileMode.S_IROTH | LinuxFileMode.S_IRGRP | LinuxFileMode.S_IRUSR | LinuxFileMode.S_IFREG, script.Mode);
             Assert.Equal("root", script.Owner);
             Assert.False(script.RemoveOnUninstall);
-            Assert.Equal("archive\\script.sh", script.SourceFilename);
+            Assert.Equal(Path.Combine("archive", "script.sh"), script.SourceFilename);
             Assert.Equal("/opt/demo/script.sh", script.TargetPath);
             Assert.Equal("/opt/demo/script.sh", script.TargetPathWithFinalSlash);
             Assert.Equal(ArchiveEntryType.None, script.Type);
@@ -79,7 +80,7 @@ namespace Packaging.Targets.Tests
             Assert.Equal(LinuxFileMode.S_IROTH | LinuxFileMode.S_IRGRP | LinuxFileMode.S_IRUSR | LinuxFileMode.S_IFREG, readme.Mode);
             Assert.Equal("root", readme.Owner);
             Assert.False(readme.RemoveOnUninstall);
-            Assert.Equal("archive\\README.md", readme.SourceFilename);
+            Assert.Equal(Path.Combine("archive", "README.md"), readme.SourceFilename);
             Assert.Equal("/opt/demo/README.md", readme.TargetPath);
             Assert.Equal("/opt/demo/README.md", readme.TargetPathWithFinalSlash);
             Assert.Equal(ArchiveEntryType.None, readme.Type);
@@ -94,7 +95,7 @@ namespace Packaging.Targets.Tests
             Assert.Equal(LinuxFileMode.S_IXOTH | LinuxFileMode.S_IROTH | LinuxFileMode.S_IXGRP | LinuxFileMode.S_IRGRP | LinuxFileMode.S_IXUSR | LinuxFileMode.S_IWUSR | LinuxFileMode.S_IRUSR, script.Mode);
             Assert.Equal("root", script.Owner);
             Assert.False(script.RemoveOnUninstall);
-            Assert.Equal("archive\\script.sh", script.SourceFilename);
+            Assert.Equal(Path.Combine("archive", "script.sh"), script.SourceFilename);
             Assert.Equal("/bin/script.sh", script.TargetPath);
             Assert.Equal("/bin/script.sh", script.TargetPathWithFinalSlash);
             Assert.Equal(ArchiveEntryType.None, script.Type);
