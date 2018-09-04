3e0cae197a735e7ae84699b695b719d20cb064c6
Frederik Carlier <frederik.carlier@quamotion.mobi>
2018-09-04 22:04:13 +0200

    Run unit tests to verify deployment

---
+++ demo/cliscd/Tests.cs
@@ -0,0 +1,44 @@
+﻿using Mono.Unix;
+using System.IO;
+using Xunit;
+
+namespace cliscd
+{
+    public class Tests
+    {
+        [Fact]
+        public void UserExists()
+        {
+            var user = new UnixUserInfo("quamotion");
+            Assert.NotEqual(0, user.UserId);
+            Assert.NotEqual(0, user.GroupId);
+            Assert.Equal("quamotion", user.GroupName);
+            Assert.Equal("/usr/share/cliscd", user.HomeDirectory);
+        }
+
+        [Fact]
+        public void MachineFileExists()
+        {
+            Assert.True(File.Exists("/etc/clifdd/cliscd.machine.config"));
+        }
+
+        [Fact]
+        public void UserFileExists()
+        {
+            Assert.True(File.Exists("/home/quamotion/.cliscd/cliscd.user.config"));
+        }
+
+        [Theory]
+        [InlineData("/var/log/quamotion")]
+        [InlineData("/var/run/quamotion")]
+        [InlineData("/var/lib/quamotion")]
+        public void LinuxFolderExists(string path)
+        {
+            Assert.True(Directory.Exists(path));
+
+            var directoryInfo = new UnixDirectoryInfo(path);
+            Assert.Equal("quamotion", directoryInfo.OwnerGroup.GroupName);
+            Assert.Equal("quamotion", directoryInfo.OwnerUser.UserName);
+        }
+    }
+}
