10e4c5d18a558ff70420ffb4a735999c2433b6f0
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-11-21 12:28:26 +0100

    RpmTask: Auto-determine package architecture

---
+++ Packaging.Targets.Tests/Rpm/RpmTaskTests.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+
+namespace Packaging.Targets.Tests.Rpm
+{
+    /// <summary>
+    /// Tests the <see cref="RpmTask"/> class.
+    /// </summary>
+    public class RpmTaskTests
+    {
+        /// <summary>
+        /// Tests the <see cref="DebTask.GetPackageArchitecture(string)"/> method.
+        /// </summary>
+        /// <param name="runtimeIdentifier">
+        /// The .NET runtime identifier.
+        /// </param>
+        /// <param name="packageAchitecture">
+        /// The expected package architecture.
+        /// </param>
+        [InlineData(null, "any")]
+        [InlineData("ubuntu.18.04", "any")]
+        [InlineData("ubuntu.18.04-x86", "i386")]
+        [InlineData("ubuntu.18.04-x64", "x86_64")]
+        [InlineData("ubuntu.18.04-arm", "armhfp")]
+        [InlineData("ubuntu.18.04-arm64", "aarch64")]
+        [InlineData("linux", "any")]
+        [InlineData("linux-x86", "i386")]
+        [InlineData("linux-x64", "x86_64")]
+        [InlineData("linux-arm", "armhfp")]
+        [InlineData("linux-arm64", "aarch64")]
+        [Theory]
+        public void GetPackageArchitectureTest(string runtimeIdentifier, string packageAchitecture)
+        {
+            Assert.Equal(packageAchitecture, RpmTask.GetPackageArchitecture(runtimeIdentifier));
+        }
+    }
+}
