b831ea354072b7c09c946c6c1c8700535c4252ab
Frederik Carlier <frederik.carlier@quamotion.mobi>
2019-11-22 16:04:47 +0100

    Add Molecule test for self-contained app

---
+++ molecule/self-contained/self-contained-app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace self_contained_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
